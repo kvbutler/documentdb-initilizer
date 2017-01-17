c9870fa5ef7572c7b75d12a1f94fc77c88519cd3
Baron Chen <baron.chen@macquarie.com>
2017-01-17 17:03:30 +1100

    more update

--- spec/lib.spec.js
+++ spec/lib.spec.js
@@ -1,15 +1,10 @@
-var fs = require('fs');
-var proxyquire =  require('proxyquire')
-var core = require('../lib/core.js');
-var lib = proxyquire('../lib/lib.js', { './core.js': core});
-
-
 describe("lib test", function () {
 
     it("should load objects from file", function (done) {
-
+        var fs = require('fs');
+        var lib = require('../lib/lib.js');
         var dir = process.cwd() + '/spec/testFiles';
-
+        
         var objs = lib.loadFiles(dir);
 
         expect(objs.length).toBe(3);
@@ -23,6 +18,10 @@ describe("lib test", function () {
 
     it("should save store proc", function (done) {
 
+        var proxyquire =  require('proxyquire')
+        var core = require('../lib/core.js');
+        var lib = proxyquire('../lib/lib.js', { './core.js': core});
+        var dir = process.cwd() + '/spec/testFiles';
         var storeProcs = [
             {id:"1"},{id:"2"},{id:"3"}
         ]
