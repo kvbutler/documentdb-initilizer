47433a8e17ae0df53a732c8b3ec9efa8739ab10a
Baron Chen <baron.chen@macquarie.com>
2017-01-18 13:06:56 +1100

    Add trigger functionanlity

--- spec/core.spec.js
+++ spec/core.spec.js
@@ -14,6 +14,15 @@ describe("core test", function () {
             response.setBody("Hello, World");
         }
     };
+    var trigger = {
+        id: "helloWorld",
+        triggerType: "Pre",
+        triggerOperation: "All",
+        serverScript: function () {
+            var context = getContext();
+            var response = context.getResponse();
+        }
+    }
 
     beforeEach(function () {
         testConfig = {
@@ -63,7 +72,7 @@ describe("core test", function () {
         }
         spyOn(fakeClient, "upsertStoredProcedureAsync").and.callThrough();
 
-        core.saveStoreProc(storeProc);
+        core.saveStoreProc(storeProc, {});
 
         expect(fakeClient.upsertStoredProcedureAsync).toHaveBeenCalledWith(testLink, storeProc);
 
@@ -77,6 +86,40 @@ describe("core test", function () {
         }, 500);
 
     })
+
+      it("should try to save one trigger", function (done) {
+        var core = require('../lib/core.js');
+        var testId = trigger.id;
+        prepareCommonSpies(core);
+        fakeClient.upsertTriggerAsync = function (obj, config) {
+            var deferred = $q.defer();
+            setTimeout(function () {
+                deferred.resolve({
+                    resource: {
+                        id: testId
+                    }
+                });
+            }, 200);
+
+            return deferred.promise;
+        }
+        spyOn(fakeClient, "upsertTriggerAsync").and.callThrough();
+
+        core.saveTrigger(trigger, {});
+
+        expect(fakeClient.upsertTriggerAsync).toHaveBeenCalledWith(testLink, trigger);
+
+        setTimeout(function () {
+            expect(console.log).toHaveBeenCalledWith("Successfully created trigger: " + testId);
+
+            fakeClient.upsertTriggerAsync.calls.reset();
+            console.log.and.stub();
+            done();
+
+        }, 500);
+
+    })
+
 
     function prepareCommonSpies(core) {
         spyOn(core, "getClient").and.returnValue(fakeClient);
--- spec/lib.spec.js
+++ spec/lib.spec.js
@@ -1,6 +1,7 @@
 describe("lib test", function () {
 
     it("should load objects from file", function (done) {
+        //leave fs not mcoked on purpose. Want to check if the file system actually work.
         var fs = require('fs');
         var lib = require('../lib/lib.js');
         var dir = process.cwd() + '/spec/testFiles';
@@ -28,18 +29,15 @@ describe("lib test", function () {
                 var context = getContext();
                 var response = context.getResponse();
 
-                response.setBody("Hello, World");
             }
         },
-        {
-            id: "helloWorld2",
-            serverScript: function () {
-                var context = getContext();
-                var response = context.getResponse();
-
-                response.setBody("Hello, World2");
-            }
-        }];
+            {
+                id: "helloWorld2",
+                serverScript: function () {
+                    var context = getContext();
+                    var response = context.getResponse();
+                }
+            }];
 
         spyOn(lib, "loadFiles").and.returnValue(storeProcs);
 
@@ -49,6 +47,44 @@ describe("lib test", function () {
 
         expect(lib.loadFiles).toHaveBeenCalledWith(dir);
         expect(core.saveStoreProc.calls.count()).toBe(storeProcs.length);
+
+        done();
+    })
+
+    it("should try to save triggers", function (done) {
+
+        var proxyquire = require('proxyquire')
+        var core = require('../lib/core.js');
+        var lib = proxyquire('../lib/lib.js', { './core.js': core });
+        var dir = 'testDir';
+        var triggers = [{
+            id: "helloWorld",
+            triggerType: "Pre",
+            triggerOperation: "All",
+            serverScript: function () {
+                var context = getContext();
+                var response = context.getResponse();
+
+            }
+        },
+        {
+            id: "helloWorld2",
+            triggerType: "Post",
+            triggerOperation: "Create",
+            serverScript: function () {
+                var context = getContext();
+                var response = context.getResponse();
+            }
+        }];
+
+        spyOn(lib, "loadFiles").and.returnValue(triggers);
+
+        spyOn(core, 'saveTrigger');
+
+        lib.createTriggers(dir, {});
+
+        expect(lib.loadFiles).toHaveBeenCalledWith(dir);
+        expect(core.saveTrigger.calls.count()).toBe(triggers.length);
 
         done();
     })
